a4681c7ae41a1659b1f223ae5b57c21ae41bef19
Emir Sağlam <Emir Salam@EMIRSAGLAM>
2024-10-14 12:02:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ProgTemelleri1
+{
+    internal class Program
+    {
+        string i = "Ahmet";
+        string s = "celik";
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("gazi");
+            Console.ReadKey();
+        }
+    }
+}
+// Bu bir açıklama satırıdır.
+// Namespace: İçinde blokları bulunduran yapılardır.
+// Class:İçinde blokları bulunduran yapılardır.
+// Metod iş yapan kod bloklarına denir.
+// Main Metod: Console uygulamalarının giriş noktasıdır
+// Ctrl + F5 : Hata ayıklama modu olmadan programı çalıştırır.
+// Syntax: Programlama dilleri yazım kuralları.
+// C# Syntax:
+// 1-Açılan parantezler mutlaka kapatılmalıdır.
+// 2-Scope tanımlamalarının sonunda : bulunmaz.
+// 3-Satırların sonunda: bulunur.
+// 4-Metodların sonunda mutlaka parantezler bulunur.
+// Syntax hatası olursa program derlenmez, dolayısıyla çalışmaz. VS hata olan yerin altını kırmızı ile çizer.
+// Bununla birlilte, scroolbar'da kırmızı bir işaretle hatanın yerini bildirir.
+// VS bildirisinin alt kısmındada kırmızı bir çarpı işareti bulunur.
+
+// IntelliSence: Kod yazarken yardımcı olan penceredir.
+// IntelliCode: Yapay zeka destekli kod yazım yardımcısı.
+
+// Solition Explorer: solutin'ımızın içindeki projele ulaşabileceğimiz penceredir.
+// View menüsü aracılığıyla açılabilir.
+// Solution: Bir ya da birden fazla proje içerebilirler.
+// CTRL + K + C : açıklama satırı
+// CTRL + K + U : açıklama satırı iptali
+// CTRL + K + . : olası çözüm yolları.
+// CTRL + K + D : Kod hiyerarşisini düzenler.
+//C# -> Derleme -> MS-IL -> Derleme(.NET Runtime) -> 0-1(Binary)  --------> 2 Farklı işlemcide de programın çalışmasını sağlar.
+//MS-IL: Misrosoft Intermediate Language
+// var sayi = 3.15D; -------> Implicit (Veri tipinin açık belirtilmediği durum)
+// double number = 3.15; ------> Explicit (Veri tipinin açıkca belirtildiği durum)
+
+
+
